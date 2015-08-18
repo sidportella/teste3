3f0e1ed4d7dd4ddf54e3f3374d655c0a8e3f7abb
sidportella <sp@yosemite.local>
2015-08-17 21:09:38 -0300

    ok

---
+++ primeiroapp_edge.js
@@ -0,0 +1,17 @@
+
+(function(compId){var _=null,y=true,n=false,x10='break-word',x11='nowrap',e13='${btnclique}',x20='ellipse',x4='both',b='block',x21='rgba(236,39,39,1.00)',x27='true',x='text',m='rect',e14='${Text2}',x5='horizontal',i='none',x3='6.0.0.400',x19='Ellipse',x16='319px',x26='rgba(255,255,255,1.00)',x18='50%',x8='Arial, Helvetica, sans-serif',p='px',l='normal',x2='5.0.0',x17='auto',x1='6.0.0',x24='Text',x23='121px',x12='rgba(255,255,255,1)',x22='71px',lf='left',x9='400',x15='0px',xc='rgba(0,0,0,1)',x7='68',d='display';var s25="<p style=\"margin: 0px;\">​clique</p>",s6="<p style=\"margin: 0px;\">​Ola Luciana Fleury!</p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:x4,cg:x5,rI:n,cn:{dom:[{id:'btnclique',symbolName:'btnclique',v:b,t:m,r:['352px','224','319','319','auto','auto'],cu:'pointer'},{id:'Text2',v:i,t:x,r:['228px','306px','auto','auto','auto','auto'],text:s6,align:"left",n:[x8,[x7,p],"rgba(53,46,46,1.00)",x9,i,l,x10,x11],ts:["","","","",i]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'1024px','768px'],overflow:'hidden',f:[x12]}}},tt:{d:1000,a:y,data:[["eid2",d,0,0,"linear",e13,b,b],["eid3",d,1000,0,"linear",e13,b,i],["eid4",d,0,0,"linear",e14,i,i],["eid5",d,1000,0,"linear",e14,i,b],["eid1",lf,0,0,"linear",e13,'352px','352px']]}},"btnclique":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x15,x15,x16,x16,x17,x17],br:[x18,x18,x18,x18],id:x19,s:[0,xc,i],t:x20,f:[x21]},{r:[x22,x23,x17,x17,x17,x17],id:x24,text:s25,n:[x8,[68,p],x26,l,i,'',x10,x11],t:x}],style:{'${symbolSelector}':{isStage:x27,r:[undefined,undefined,x16,x16]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-370258581");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${btnclique}","click",function(sym,e){sym.stop(1000);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'btnclique'
+(function(symbolName){})("btnclique");
+//Edge symbol end:'btnclique'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-370258581");
